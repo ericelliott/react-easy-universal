01956cc493bde443e85d7016b0d77409fd63ecbe
Eric Elliott <support@paralleldrive.com>
2016-01-25 19:04:47 -0700

    Create client.js

---
+++ test/fixtures/client.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import createApp from './path/to/create-app.js';
+
+// returns a function that must be invoked to trigger render
+const app = createApp({ React }); // use all the defaults
+
+export default app;
