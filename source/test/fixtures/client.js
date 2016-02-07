e6de7320a3208e09b2dad2c66835998574c86b23
Eric Elliott <support@paralleldrive.com>
2016-02-07 00:35:18 -0700

    Prep for initial code publish.

--- source/test/fixtures/client.js
+++ source/test/fixtures/client.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import createApp from './wire-app.js';
+import wireApp from './wire-app.js';
 
 // returns a function that must be invoked to trigger render
-const app = createApp({ React }); // use all the defaults
+const app = wireApp({ React }); // use all the defaults
 
+// The app function will return your store so you can dispatch actions.
 const store = app();
 
+// Do stuff in your client app to trigger re-renders.
+// e.g., subscribe to server updates, etc...
 store.dispatch({
   type: 'SET_TITLE',
   title: 'Client render'
