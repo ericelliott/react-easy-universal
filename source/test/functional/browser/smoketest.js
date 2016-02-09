1f1203d39db523c9c5743f69691fb9a80c21c5ff
Eric Elliott <support@paralleldrive.com>
2016-02-09 00:06:34 -0700

    Add test delay for CI

--- source/test/functional/browser/smoketest.js
+++ source/test/functional/browser/smoketest.js
@@ -6,6 +6,7 @@ module.exports = {
     browser
       .url(`http://localhost:${NODE_PORT}/`)
       .waitForElementVisible('body', WAIT)
+      .pause(100)
       .assert.containsText('body', 'Client render')
       .end();
   }
